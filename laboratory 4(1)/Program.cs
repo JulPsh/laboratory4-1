4d6a338dbd76f52ce44c9b437206c06d278b90b6
JulPsh <6523+JulPsh@users.noreply.github.com>
2019-12-15 22:54:34 +0300

    laboratory4-1

---
+++ laboratory 4(1)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace laboratory_4_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            Console.WriteLine("Введите число m");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число p");
+            int p = Convert.ToInt32(Console.ReadLine());
+            if (m < 0)
+                ++s;
+            if (n < 0)
+                ++s;
+            if (p < 0)
+                ++s;
+            Console.Write("Отрицательных чисел {0}", s);
+            Console.ReadLine();
+        }
+    }
+}
